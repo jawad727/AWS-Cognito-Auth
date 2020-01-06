6e2072e8d732c68c8b31bfe62a4c10e97a4d6f00
jawad727 <jawaddhussein@gmail.com>
2020-01-05 22:52:42 -0800

    landing page second half done in widescreed

--- src/components/landingpage/LandingPage.js
+++ src/components/landingpage/LandingPage.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './landing.css';
 import img from "./pictures/landing-image2.png"
 import img2 from "./pictures/landing-image.png"
+import img3 from "./pictures/wayve.png"
 
 class LandingPage extends Component {
 
@@ -10,26 +11,34 @@ class LandingPage extends Component {
         
         return (
             <div className="landingContainer">
-                <div>
+                <div className="topDiv">
                   <p className="LandingTitle" >Your personal news feed delivered</p>
                   <p className="LandingSlogan" > Read your favorite articles. </p>
                   <p className="LandingSlogan" > Connect with friends. </p>
                 </div>
                 <img className="firstHalfImage" src={img}/>
-                <button className="firstHalfButton" > Sign Up </button>
+                <button className="firstHalfButton" > Browse All Articles </button>
+
+                <img className="wayve" src={img3} />
              
               <div className="secondHalfDiv" >
-                <img className="secondHalfImage" src={img2}/>
-                <div className="secondHalfContent">
-                  <p className="secondHalfTitle"> Your New Social News Hub </p>
-                  <p className="secondHalfParagraph"> ArticleHub puts the news in your hands. 
-                  Browse your favorite articles across politics, sports, tech, art, and more! </p>
-                  <p className="secondHalfParagraph"> Connect with other users to see articles they're
-                   posting and exchange ideas in comments </p>
-                  <p className="secondHalfParagraph"> Curate your news feed. Tired of old articles? Delete
-                   them from your profile to make room for new and exciting reads!</p>
-                   <button> Sign Up </button>
-                </div>
+                  <div className="SecondHalfInnerContainer">
+                    <img className="secondHalfImage" src={img2}/>
+                    <div className="secondHalfContent">
+                      <p className="secondHalfTitle"> Your New Social News Hub </p>
+                      <p className="secondHalfParagraph"> ArticleHub puts the news in your hands. 
+                      Browse your favorite articles across politics, sports, tech, art, and more! </p>
+                      <p className="secondHalfParagraph"> Connect with other users to see articles they're
+                      posting and exchange ideas in comments </p>
+                      <p className="secondHalfParagraph"> Curate your news feed. Tired of old articles? Delete
+                      them from your profile to make room for new and exciting reads!</p>
+                      <button> Sign Up </button>
+                    </div>
+                  </div>
+              </div>
+
+              <div className="LandingFooter" >
+                <p>Copyright 2019 ArticleHub, Inc.</p>
               </div>
 
             </div>
