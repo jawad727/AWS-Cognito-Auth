3d6453278e7dbe237931e2d2ce201f6fae01296c
jawad727 <jawaddhussein@gmail.com>
2020-01-16 23:43:09 -0800

    all attributes on state being tested on type change

--- src/store/reducers/index.test.js
+++ src/store/reducers/index.test.js
@@ -1,12 +1,13 @@
 import { rootReducer } from "./index.js"
 import { UPDATE_USER, POST_LIKE, FETCH_LIKES, FETCH_SINGLE_USER, POST_COMMENT, FETCH_USERS, FETCH_COMMENTS, POST_USER, FETCH_POSTS, MAKE_POST, DELETE_POST, FETCH_POST_BY_USER } from "../actions/index"
 
+const posts = [{title: "Test 1"}, {title: "Test 2"}, {title: "Test 3"}]
+
 describe("Root reducer", () => {
 
     it("should return default state", () => {
         const newState = rootReducer(undefined, {})
         expect(newState).toEqual({
-                signedIn: false,
                 usersArray: [],
                 allPostsArray: [],
                 allPostsByUser: [],
@@ -15,6 +16,7 @@ describe("Root reducer", () => {
                 singleUser: [],
                 userFetched: false
             })
+        console.log(newState)
     })
 
 
@@ -22,7 +24,6 @@ describe("Root reducer", () => {
 
         it("Should return new state if receiving type", () => {
 
-            const posts = [{title: "Test 1"}, {title: "Test 2"}, {title: "Test 3"}]
             const newState = rootReducer(undefined, {
                 type: FETCH_SINGLE_USER,
                 payload: posts
@@ -37,7 +38,6 @@ describe("Root reducer", () => {
 
         it("Should return new state if receiving type", () => {
 
-            const posts = [{title: "Test 1"}, {title: "Test 2"}, {title: "Test 3"}]
             const newState = rootReducer(undefined, {
                 type: FETCH_USERS,
                 payload: posts
@@ -47,15 +47,67 @@ describe("Root reducer", () => {
     })
 
 
-})
+    describe(".allPostsArray", () => {
 
-// {
-//     signedIn: false,
-//     usersArray: [],
-//     allPostsArray: [],
-//     allPostsByUser: [],
-//     postComments: [],
-//     likesArray: [],
-//     singleUser: [],
-//     userFetched: false
-// }
+        it("Should return new state if receiving type", () => {
+
+            const newState = rootReducer(undefined, {
+                type: FETCH_POSTS,
+                payload: posts
+            })
+            expect(newState.allPostsArray).toEqual(posts);
+        })
+    })
+
+    
+    describe(".allPostsByUser", () => {
+
+        it("Should return new state if receiving type", () => {
+
+            const newState = rootReducer(undefined, {
+                type: FETCH_POST_BY_USER,
+                payload: posts
+            })
+            expect(newState.allPostsByUser).toEqual(posts);
+        })
+    })
+
+    
+    describe(".postComments", () => {
+
+        it("Should return new state if receiving type", () => {
+
+            const newState = rootReducer(undefined, {
+                type: FETCH_COMMENTS,
+                payload: posts
+            })
+            expect(newState.postComments).toEqual(posts);
+        })
+    })
+
+    describe(".likesArray", () => {
+
+        it("Should return new state if receiving type", () => {
+
+            const newState = rootReducer(undefined, {
+                type: FETCH_LIKES,
+                payload: posts
+            })
+            expect(newState.likesArray).toEqual(posts);
+        })
+    })
+
+
+    describe(".singleUser", () => {
+
+        it("Should return new state if receiving type", () => {
+
+            const newState = rootReducer(undefined, {
+                type: FETCH_SINGLE_USER,
+                payload: posts
+            })
+            expect(newState.singleUser).toEqual(posts);
+        })
+    })
+
+})
